0752135372e35c10a30fdd36858ad4187744a192
maya douadi <maya.douadi@outlook.fr>
2019-08-16 11:30:42 +0200

    yeye

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
 
 20.times do
-  cock = Cocktail.new(name: Faker::Commerce.unique.product_name, description: Faker::Lorem.sentence, alcohol: rand(0..1))
+  url = 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiP6fG6iIfkAhUDJhoKHfzyBRIQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cuisineaz.com%2Frecettes%2Fcocktail-de-champagne-53419.aspx&psig=AOvVaw12BfPy_ob9meAPn9gLd8RT&ust=1566033732760921'
+  cock = Cocktail.new(name: Faker::Commerce.unique.product_name, description: Faker::Lorem.sentence, alcohol: rand(0..1), photo: url)
   rand(3..8).times do
     ingre = Ingredient.new(name: Faker::Food.unique.ingredient)
     dos = Dose.new(description: "5cl", cocktail: cock, ingredient: ingre)
